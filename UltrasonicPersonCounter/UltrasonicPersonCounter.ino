aa02405c3fe859f77a866055441a772e79bc27f9
kbaturalp <60824816+Aliennor@users.noreply.github.com>
2022-08-12 23:13:42 +0300

    Update UltrasonicPersonCounter.ino

--- UltrasonicPersonCounter/UltrasonicPersonCounter.ino
+++ UltrasonicPersonCounter/UltrasonicPersonCounter.ino
@@ -1,4 +1,5 @@
 #include <LiquidCrystal.h>
+
 #define echoPin 8
 #define trigPin 9 
 
@@ -6,9 +7,12 @@
 
   const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
   LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
   long duration;
   int distance;
-	
+  int count=0;
+  int state=1;
+  int laststate=1;
 
 int counter() {
   
@@ -28,11 +32,15 @@ int counter() {
   lcd.print("Distance: ");
   lcd.print(distance);
   lcd.print(" cm");
-  if(distance<50){
-    return 1;
-   }
-  else
-    return 0;
+  
+    if(distance<50 && state==1){
+      state=0;
+      return 1;
+     }
+    else{
+      state=1;    
+      return 0;
+     }
   }
 
 
@@ -48,11 +56,12 @@ int counter() {
 
  void loop() {
    
-   int count = 0;
+   
    count = count + counter();
    
    lcd.setCursor(0 ,1);
    
    lcd.print("Visitors : ");
    lcd.print(count);
+   delay(2000);
    } 
